6960acad49c689754665ca5e6601f02a2d800526
= <=>
2025-10-07 21:09:05 +0700

    Membuat Halaman Dashboard

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="id">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Dashboard</title>
+  <link rel="stylesheet" href="{{asset('css/dashboard.css')}}">
+</head>
+
+<body>
+  <div class="judul">Dashboard</div>
+
+  <nav>
+    <ul class="nav-left">
+      <li><a href="#">Dashboard</a></li>
+      <li><a href="#">Info</a></li>
+    </ul>
+    <ul class="nav-right">
+      <li><a href="#">Logout</a></li>
+    </ul>
+  </nav>
+</body>
+
+</html>
