ba2e2b128116f68cbb0aaf411f83344c7497a2fc
Alfifauzan14 <alfifauzan897@gmail.com>
2025-10-09 11:16:12 +0700

    Manambahkan Pelanggan

--- resources/views/pelanggan.blade.php
+++ resources/views/pelanggan.blade.php
@@ -1,21 +1,25 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="id">
 
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Pelanggan</title>
+  <link rel="stylesheet" href="{{asset('css/dashboard.css')}}">
 </head>
 
 <body>
-  <h1>Welcome Profile!</h1>
-  <ul>
-    <li><a href="{{ url('/') }}">Home</a></li>
-    <li><a href="{{ url('/about') }}">About</a></li>
-    <li><a href="{{ url('/profile') }}">Profile</a></li>
-    <li><a href="{{ url('/siswa') }}">Siswa</a></li>
-    <li><a href="{{ url('/kelas') }}">Kelas</a></li>
-  </ul>
+  <div class="judul">Pelanggan</div>
+
+  <nav>
+    <ul class="nav-left">
+      <li><a href="#">Dashboard</a></li>
+      <li><a href="#">Info</a></li>
+    </ul>
+    <ul class="nav-right">
+      <li><a href="#">Logout</a></li>
+    </ul>
+  </nav>
 </body>
 
 </html>
